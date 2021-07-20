f096cbdcc5c7e792d85b2a2d9011fae10e0751c4
kanatamon <ponde.moonlord@gmail.com>
2021-07-20 14:04:03 +0700

    Tweaks to main view

--- src/components/ShoeIndex/ShoeIndex.js
+++ src/components/ShoeIndex/ShoeIndex.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from 'react'
+import styled from 'styled-components/macro'
 
-import { WEIGHTS } from '../../constants';
+import { WEIGHTS, QUERIES } from '../../constants'
 
-import Breadcrumbs from '../Breadcrumbs';
-import Select from '../Select';
-import Spacer from '../Spacer';
-import ShoeSidebar from '../ShoeSidebar';
-import ShoeGrid from '../ShoeGrid';
+import Breadcrumbs from '../Breadcrumbs'
+import Select from '../Select'
+import Spacer from '../Spacer'
+import ShoeSidebar from '../ShoeSidebar'
+import ShoeGrid from '../ShoeGrid'
 
 const ShoeIndex = ({ sortId, setSortId }) => {
   return (
@@ -15,57 +15,74 @@ const ShoeIndex = ({ sortId, setSortId }) => {
       <MainColumn>
         <Header>
           <Title>Running</Title>
-          <Select
-            label="Sort"
+          <ResponsiveSelect
+            label='Sort'
             value={sortId}
             onChange={(ev) => setSortId(ev.target.value)}
           >
-            <option value="newest">Newest Releases</option>
-            <option value="price">Price</option>
-          </Select>
+            <option value='newest'>Newest Releases</option>
+            <option value='price'>Price</option>
+          </ResponsiveSelect>
         </Header>
         <Spacer size={32} />
         <ShoeGrid />
       </MainColumn>
       <LeftColumn>
         <Breadcrumbs>
-          <Breadcrumbs.Crumb href="/">Home</Breadcrumbs.Crumb>
-          <Breadcrumbs.Crumb href="/sale">Sale</Breadcrumbs.Crumb>
-          <Breadcrumbs.Crumb href="/sale/shoes">
-            Shoes
-          </Breadcrumbs.Crumb>
+          <Breadcrumbs.Crumb href='/'>Home</Breadcrumbs.Crumb>
+          <Breadcrumbs.Crumb href='/sale'>Sale</Breadcrumbs.Crumb>
+          <Breadcrumbs.Crumb href='/sale/shoes'>Shoes</Breadcrumbs.Crumb>
         </Breadcrumbs>
         <Spacer size={42} />
         <ShoeSidebar />
       </LeftColumn>
     </Wrapper>
-  );
-};
+  )
+}
 
 const Wrapper = styled.div`
   display: flex;
   flex-direction: row-reverse;
   align-items: baseline;
   gap: 32px;
-`;
+
+  @media ${QUERIES.tabletAndDown} {
+    flex-direction: column-reverse;
+    gap: 8px;
+  }
+`
 
 const LeftColumn = styled.div`
   flex-basis: 248px;
-`;
+
+  @media ${QUERIES.tabletAndDown} {
+    flex: 1 1 0;
+
+    & > ${Spacer} {
+      display: none;
+    }
+  }
+`
 
 const MainColumn = styled.div`
   flex: 1;
-`;
+`
+
+const ResponsiveSelect = styled(Select)`
+  @media ${QUERIES.phoneAndDown} {
+    display: none;
+  }
+`
 
 const Header = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: baseline;
-`;
+`
 
 const Title = styled.h2`
   font-size: 1.5rem;
   font-weight: ${WEIGHTS.medium};
-`;
+`
 
-export default ShoeIndex;
+export default ShoeIndex
