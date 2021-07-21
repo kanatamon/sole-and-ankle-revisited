65fec589dcfb9823377cdfedc2f53b137f0e76da
kanatamon <ponde.moonlord@gmail.com>
2021-07-21 15:18:13 +0700

    Apply solution on mobile menu modal

--- src/components/MobileMenu/MobileMenu.js
+++ src/components/MobileMenu/MobileMenu.js
@@ -2,7 +2,6 @@
 import React from 'react'
 import styled from 'styled-components/macro'
 import { DialogOverlay, DialogContent } from '@reach/dialog'
-import '@reach/dialog/styles.css'
 
 import { QUERIES, COLORS, WEIGHTS } from '../../constants'
 
@@ -16,40 +15,46 @@ const MobileMenu = ({ isOpen, onDismiss }) => {
   }
 
   return (
-    <ResponsiveDialogOverlay>
-      <StyledDialogContent aria-label='Menu'>
-        <StyledIcon id='close' onClick={onDismiss} />
+    <Overlay>
+      <Content aria-label='Menu'>
+        <CloseButton onClick={onDismiss}>
+          <Icon id='close' />
+          <VisuallyHidden>Dismiss menu</VisuallyHidden>
+        </CloseButton>
+        <Filler />
         <Nav>
-          <a href='/sale'>Sale</a>
-          <a href='/new'>New&nbsp;Releases</a>
-          <a href='/men'>Men</a>
-          <a href='/women'>Women</a>
-          <a href='/kids'>Kids</a>
-          <a href='/collections'>Collections</a>
+          <NavLink href='/sale'>Sale</NavLink>
+          <NavLink href='/new'>New&nbsp;Releases</NavLink>
+          <NavLink href='/men'>Men</NavLink>
+          <NavLink href='/women'>Women</NavLink>
+          <NavLink href='/kids'>Kids</NavLink>
+          <NavLink href='/collections'>Collections</NavLink>
         </Nav>
         <Footer>
-          <a href='/terms'>Terms and Conditions</a>
-          <a href='/privacy'>Privacy Policy</a>
-          <a href='/contact'>Contact Us</a>
+          <FooterLink href='/terms'>Terms and Conditions</FooterLink>
+          <FooterLink href='/privacy'>Privacy Policy</FooterLink>
+          <FooterLink href='/contact'>Contact Us</FooterLink>
         </Footer>
-      </StyledDialogContent>
-    </ResponsiveDialogOverlay>
+      </Content>
+    </Overlay>
   )
 }
 
-const ResponsiveDialogOverlay = styled(DialogOverlay)`
-  display: none;
-
-  @media ${QUERIES.tabletAndSmaller} {
-    display: revert;
-  }
+const Overlay = styled(DialogOverlay)`
+  position: fixed;
+  top: 0;
+  right: 0;
+  left: 0;
+  bottom: 0;
+  background: hsla(220deg 5% 40% / 0.8);
+  display: flex;
+  justify-content: flex-end;
 `
 
-const StyledDialogContent = styled(DialogContent)`
-  margin: 0;
-  margin-left: auto;
-  height: 100%;
+const Content = styled(DialogContent)`
+  background: var(--color-white);
   width: 300px;
+  height: 100%;
   padding: 26px 16px 32px 32px;
 
   display: flex;
@@ -57,36 +62,43 @@ const StyledDialogContent = styled(DialogContent)`
   justify-content: space-between;
 `
 
-const StyledIcon = styled(Icon)`
-  width: 24px;
-  height: 24px;
-  align-self: flex-end;
+const CloseButton = styled(UnstyledButton)`
+  position: absolute;
+  top: 16px;
+  right: 6px;
+  padding: 10px;
 `
 
 const Nav = styled.nav`
   display: flex;
   flex-direction: column;
   gap: 22px;
+`
 
-  & > a {
-    text-transform: uppercase;
-    text-decoration: none;
-    color: var(--color-gray-900);
-    font-weight: var(--weight-medium);
-    font-size: 1.125rem;
-  }
+const NavLink = styled.a`
+  text-transform: uppercase;
+  text-decoration: none;
+  color: var(--color-gray-900);
+  font-weight: var(--weight-medium);
+  font-size: 1.125rem;
+`
+
+const Filler = styled.div`
+  flex: 1;
 `
 
 const Footer = styled.footer`
+  flex: 1;
   display: flex;
   flex-direction: column;
+  justify-content: flex-end;
   gap: 14px;
+`
 
-  & > a {
-    text-decoration: none;
-    color: var(--color-gray-700);
-    font-size: ${14 / 16}rem;
-  }
+const FooterLink = styled.a`
+  text-decoration: none;
+  color: var(--color-gray-700);
+  font-size: ${14 / 16}rem;
 `
 
 export default MobileMenu
